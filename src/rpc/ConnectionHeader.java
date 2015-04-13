86df838f4d9aeadacb8c24bbb6990479d5fa2acd
njutony1991 <jlutony1991@gmail.com>
2015-04-13 15:37:50 +0800

    implementing the connection process

--- src/rpc/ConnectionHeader.java
+++ src/rpc/ConnectionHeader.java
@@ -1,7 +1,14 @@
 package rpc;
 
+import java.io.Serializable;
+
 /**
  * Created by tony on 15-4-10.
  */
-public class ConnectionHeader {
+public class ConnectionHeader implements Serializable{
+
+    
+    public ConnectionHeader() {
+
+    }
 }
